189cd360685f8099fb9dc9c5679d18b2f02a43c1
deepak <deepakmane33@gmail.com>
2025-08-03 21:12:29 +0530

    spring security

--- src/main/java/com/example/SpringSecurityDemo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/config/SpringSecurityConfig.java
@@ -2,10 +2,12 @@ package com.example.SpringSecurityDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +17,11 @@ public class SpringSecurityConfig {
     public UserDetailsService userDetailsService(){
         return new InMemoryUserDetailsManager();
     }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
 
 
 }
