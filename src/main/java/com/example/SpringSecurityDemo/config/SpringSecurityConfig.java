37e9034c80fcc8c8e415762ca87a271346aa654c
deepak <deepakmane33@gmail.com>
2025-08-03 21:08:49 +0530

    spring security

---
+++ src/main/java/com/example/SpringSecurityDemo/config/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.SpringSecurityDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    /*@Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager();
+    }*/
+
+
+}
